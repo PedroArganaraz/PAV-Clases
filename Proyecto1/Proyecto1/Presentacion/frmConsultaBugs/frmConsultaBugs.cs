756bea5e7f18ed9c145cc63b7dea1c5c2f515094
PedroArganaraz <111023060+PedroArganaraz@users.noreply.github.com>
2022-08-18 13:37:49 -0300

    Add files via upload

---
+++ Proyecto1/Proyecto1/Presentacion/frmConsultaBugs/frmConsultaBugs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppBTS.Presentacion
+{
+    public partial class frmConsultaBugs : Form
+    {
+      
+        public frmConsultaBugs()
+        {
+            InitializeComponent();
+        }
+
+        private void frmConsultaBugs_Load(object sender, EventArgs e)
+        {
+            this.dtpFechaDesde.Value = DateTime.Today.AddDays(-7);
+            this.dtpFechaHasta.Value = DateTime.Today;
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDetalle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
